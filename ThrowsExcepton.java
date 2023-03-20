0aa868d00e364eeff5bc9cf831660ebb7dee6bd1
Adarsh <thakuradarsh@gmail.com>
2023-03-20 15:25:15 +0530

    Handling Exception by throws Keyword

---
+++ ThrowsExcepton.java
@@ -0,0 +1,7 @@
+public class ThrowsExcepton  {
+    public static void main(String args[]) throws InterruptedException{
+        Thread.sleep(100);
+        System.out.println("Use of throws");
+    }
+    
+}
